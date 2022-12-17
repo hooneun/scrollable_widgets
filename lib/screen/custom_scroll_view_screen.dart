26986285172622210c2025f13bf97a60ea3f2ccc
hooneun <kimhoon0316@gmail.com>
2022-12-17 18:01:59 +0900

    custom scroll view

---
+++ lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  CustomScrollViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('CustomScrollViewScreen'),
+          ),
+          renderBuilderSliverList(),
+        ],
+      ),
+    );
+  }
+
+  // ListView.builder 생성장와 유사함
+  SliverList renderBuilderSliverList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        childCount: 100,
+      ),
+    );
+  }
+
+  // ListView 기본 생성자랑 비슷함
+  SliverList renderChildSliverList() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                color: rainbowColors[e % rainbowColors.length],
+                index: e,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    return Container(
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
