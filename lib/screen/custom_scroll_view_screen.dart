e0cd1f6534faa312df37007847d470cb27d0c6ce
hooneun <kimhoon0316@gmail.com>
2022-12-19 22:11:10 +0900

    sliver fixed header

--- lib/screen/custom_scroll_view_screen.dart
+++ lib/screen/custom_scroll_view_screen.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/const/colors.dart';
 
+class _SliverFixedHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+  final double maxHeight;
+  final double minHeight;
+
+  _SliverFixedHeaderDelegate({
+    required this.child,
+    required this.maxHeight,
+    required this.minHeight,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return SizedBox.expand(
+      child: child,
+    );
+  }
+
+  @override
+  // 최대 높이
+  double get maxExtent => maxHeight;
+
+  @override
+  // 최소 높이
+  double get minExtent => minHeight;
+
+  @override
+  // covariant - 상속된 클래스도 사용 가능
+  // oldDelegate - build 가 실행이 됐을때 이전 Delegate
+  // this - 새로운 delegate
+  // shouldRebuild - 새로 build 를 해야 될지 말지 결정
+  // false - build 안함, true - 다시 빌드함
+  bool shouldRebuild(_SliverFixedHeaderDelegate oldDelegate) {
+    return oldDelegate.minHeight != minHeight ||
+        oldDelegate.maxHeight != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
+
 class CustomScrollViewScreen extends StatelessWidget {
-  final List<int> numbers = List.generate(100, (index) => index);
+  final List<int> numbers = List.generate(15, (index) => index);
 
   CustomScrollViewScreen({Key? key}) : super(key: key);
 
@@ -11,12 +54,61 @@ class CustomScrollViewScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            title: Text('CustomScrollViewScreen'),
-          ),
+          renderSliverAppbar(),
+          renderHeader(),
+          renderBuilderSliverList(),
+          renderHeader(),
           renderSliverGridBuilder(),
+          renderHeader(),
+          renderChildSliverList(),
         ],
       ),
+    );
+  }
+
+  SliverPersistentHeader renderHeader() {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverFixedHeaderDelegate(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              '신기하지~',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        minHeight: 50,
+        maxHeight: 200,
+      ),
+    );
+  }
+
+  // AppBar
+  SliverAppBar renderSliverAppbar() {
+    return SliverAppBar(
+      // 스크롤 했을 때 리스트의 중간에도 AppBar가 내려온다.
+      floating: true,
+      // 완전 고정
+      pinned: false,
+      // 자석효과
+      // floating true 에서만 사용 가능
+      snap: true,
+      // 맨 위에서 한계 이상으로 스크롤 했을때
+      // 남는 공간을 차지
+      stretch: true,
+      expandedHeight: 200,
+      collapsedHeight: 150,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text('FlexibleSpace'),
+        background: Image.asset(
+          'assets/images/sun.png',
+        ),
+      ),
+      title: Text('CustomScrollViewScrenn'),
     );
   }
 
